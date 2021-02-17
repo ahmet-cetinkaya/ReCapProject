b23b6ebcddedfa3c64b81f787e834d614cc1bcc8
Ahmet Çetinkaya <ahmetcetinkaya4@outlook.com>
2021-02-17 13:56:27 +0300

    Add Core Project as file instead reference

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Core.Entities;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public bool Success { get; }
+        public string Message { get; }
+
+        public Result(bool success, string message) : this(success)
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+    }
+}
